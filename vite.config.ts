b67b9b845043e8c9d28542f247f5e7c693a99860
vgtbdgxt <tongxu6399@gmail.com>
2025-12-05 17:23:00 -0800

    修复 map 页面的资源路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project_team06_new/',
+  base: '/project_team06_new/map/',
   build: {
     outDir: 'map/dist',
   },
